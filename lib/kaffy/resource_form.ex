313e584c0a214d7e9e3b6fcdd0b90f76749b45f1
Matteo Gheri <57362031+matteo-gheri-pocketworks@users.noreply.github.com>
2025-03-10 16:16:44 +0000

    test

--- lib/kaffy/resource_form.ex
+++ lib/kaffy/resource_form.ex
@@ -134,7 +134,7 @@ defmodule Kaffy.ResourceForm do
       :binary_id ->
         case Kaffy.ResourceSchema.primary_key(schema) == [field] do
           true -> text_input(form, field, opts)
-          false -> text_or_assoc(conn, schema, form, field, opts)
+          false -> text_or_assoc(conn, schema, form, field, opts, options)
         end
 
       :string ->
@@ -348,7 +348,7 @@ defmodule Kaffy.ResourceForm do
     ]
   end
 
-  defp text_or_assoc(conn, schema, form, field, opts) do
+  defp text_or_assoc(conn, schema, form, field, opts, options_field \\ []) do
     actual_assoc =
       Enum.filter(Kaffy.ResourceSchema.associations(schema), fn a ->
         Kaffy.ResourceSchema.association(schema, a).owner_key == field
@@ -417,7 +417,7 @@ defmodule Kaffy.ResourceForm do
               |> Enum.at(0)
 
             string_field =
-              if Map.get(opts, :use_id, false) do
+              if options_field[:use_id] do
                 :id
               else
                 case is_nil(popular_strings) do
